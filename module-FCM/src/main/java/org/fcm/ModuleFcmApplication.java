e6d9eead46d528af2c15902cce6135a0bf2347a8
joon <khjoon372@naver.com>
2024-06-14 16:06:42 +0900

    :rocket: project setting

---
+++ module-FCM/src/main/java/org/fcm/ModuleFcmApplication.java
@@ -0,0 +1,13 @@
+package org.fcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleFcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleFcmApplication.class, args);
+    }
+
+}
---
+++ module-FCM/src/test/java/org/fcm/ModuleFcmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleFcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
